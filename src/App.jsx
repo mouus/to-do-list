85eee6d4b77da882dcebd77765ee4b3e591d7fde
mouus <musannif.git@gmail.com>
2023-06-02 04:13:31 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,82 @@
+import React, { useEffect, useState } from 'react'
+import "./style.css"
+
+function App() {
+  const [newItem, setNewItem] = useState("")
+  const [todos, setTodos] = useState(()=>{
+    const localValue = localStorage.getItem("ITEMS")
+    if(localValue === null) {return[]}
+    return JSON.parse(localValue)
+  })
+
+  useEffect(()=> {
+    localStorage.setItem("ITEMS", JSON.stringify(todos))
+  },[todos])
+
+  function handleSubmit(e){
+    e.preventDefault()
+    if (!newItem.trim()) {
+      return; 
+    }
+    setTodos((currentTodos)=> {
+      return [   
+        ...currentTodos,
+        {id:crypto.randomUUID(), title:newItem.trim(), completed:false},
+      ]
+    })
+    setNewItem("")
+    
+  }
+  function toggleTodo(id, completed) {
+    setTodos(currentTodos => {
+      return currentTodos.map(todo => {
+        if (todo.id === id) {
+          return {...todo, completed}
+        }
+        return todo
+      })
+    })
+  }
+
+  function deleteTodos(id){
+    setTodos(currentTodos => {
+      return currentTodos.filter(todo => todo.id !== id)
+    })
+  }
+
+  return (
+    <>
+    <form onSubmit={handleSubmit} className="new-item-form">
+        <div className="form-row">
+          <label htmlFor="item">New Item</label>
+          <input 
+          value={newItem}
+          onChange={e => setNewItem(e.target.value)}
+          type="text"
+           id="item"/>
+        </div>
+        <button className="btn">Add</button>
+    </form>
+    <h1 className="header">Todo List</h1>
+    <ul className="list">
+    {todos.length === 0 && "No Todos"}
+    {todos.map(todo => {
+      return <li key={todo.id}>
+                <label>
+                  <input type="checkbox"  
+                  checked={todo.completed}
+                  onChange={e=> toggleTodo(todo.id, e.target.checked)}
+
+                  />
+                  {todo.title}
+                </label>
+                <button  onClick={()=> deleteTodos(todo.id)} className="btn btn-danger">Delete</button>
+             </li>
+    })}
+      
+    </ul>
+    </>
+  )
+}
+
+export default App
